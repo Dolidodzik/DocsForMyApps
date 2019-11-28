099f826d8169bafcd43d3f88a43774ced85d59a4
Dominik Lech <teethtard321@gmail.com>
2019-11-28 17:41:34 +0000

    Did things around header component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+/* Standard page elements imports */
+import { HeaderComponent } from '../header/header.component';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,6 +11,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+
+  Header:string = 'This text is passed to child';
 
   ngOnInit() {
   }
