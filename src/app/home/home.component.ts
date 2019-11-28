6ff6fdd4bcd76b419c486b74bca3a7e5806cec3b
Dominik Lech <teethtard321@gmail.com>
2019-11-28 17:52:38 +0000

    Did things around text component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 /* Standard page elements imports */
 import { HeaderComponent } from '../header/header.component';
+import { TextComponent } from './text/text.component';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,8 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  Header:string = 'This text is passed to child';
+  Header:string = 'Welcome to MyDocs';
+  Text:string = 'This site is simple styled, piece of documentation for my own, open-source projects. Here you can read how my code works';
 
   ngOnInit() {
   }
--- src/app/text/text.component.ts
+++ src/app/text/text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-text',
@@ -6,10 +6,5 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text.component.sass']
 })
 export class TextComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  @Input() text:string;
 }
